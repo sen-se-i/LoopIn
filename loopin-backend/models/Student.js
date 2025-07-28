a9284b5a355e50914b40657204d7546018ef9686
sen-se-i <fahim.ahammad666@gmail.com>
2025-07-28 18:31:09 +0600

    for sending email

--- loopin-backend/models/Student.js
+++ loopin-backend/models/Student.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   name: String,
+  
   university: String,
   department: String,
   session: String,
   birthday: String,
   phone: { type: String, unique: true },
+  email: { type: String, unique: true, required: true },
   password: String,
   interests: [String]
 });
